41742ad39c922eeae4e8a94928415531334d03cc
Lazar Sidor <lazar.sidor@gmail.com>
2022-06-06 13:08:14 +0300

    Updated min supported version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "iosplusAuthentication",
+    platforms: [.iOS(SupportedPlatform.IOSVersion.v11)],
     products: [
         .library(
             name: "iosplusFirebaseAuthentication",
@@ -20,12 +21,12 @@ let package = Package(
         .package(
           name: "Firebase",
           url: "https://github.com/firebase/firebase-ios-sdk",
-          "8.0.0" ..< "9.0.0"
+          "9.0.0" ..< "10.0.0"
         ),
         .package(
           name: "Facebook",
           url: "https://github.com/facebook/facebook-ios-sdk",
-          "12.2.1" ..< "13.0.0"
+          "13.2.0" ..< "14.0.0"
         ),
         .package(
           name: "GoogleSignIn",
--- Tests/iosplusAuthenticationTests/iosplusAuthenticationTests.swift
+++ Tests/iosplusAuthenticationTests/iosplusAuthenticationTests.swift
@@ -1,11 +1,10 @@
 import XCTest
-@testable import iosplus_authentication
 
 final class iosplus_authenticationTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(iosplus_authentication().text, "Hello, World!")
+        XCTAssertEqual(1, 1)
     }
 }
